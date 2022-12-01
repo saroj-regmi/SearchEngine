9c6dea90cbf35ae0020b04c1ff54afc4ecd11961
Saroj Regmi <94213188+saroj-regmi@users.noreply.github.com>
2022-12-01 05:34:08 +0000

    CHECKPOINT: links title current link and cover image single iste

--- web-crawler/index.js
+++ web-crawler/index.js
@@ -4,15 +4,16 @@
 // dotenv.config({});
 
 // the popular sites that are scrapped first.
-const popularSites = require("./popularsites.json");
+// const popularSites = require("./popularsites.json");
 
 // for scrapping the web
 const puppeteer = require("puppeteer");
 
 const startScrapping = async () => {
   let scrappedData = {
+    title: "",
     link: "", // this will store the actual link of the site
-    coverImage: "", // the cover image of that will be showed when it is queried
+    coverImage: {}, // the cover image of that will be showed when it is queried
     allText: "", // contains all the site text information
     links: [], // contains all the other links that can reused to scrapp more information and then update the database.
     Description: "", // contains a short description of the site that is queried
@@ -24,20 +25,43 @@ const startScrapping = async () => {
   const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
+  const currentLink = "https://commons.wikimedia.org/wiki/Main_Page";
   try {
-    await page.goto("https://commons.wikimedia.org/wiki/Main_Page");
+    await page.goto(currentLink);
 
     // contains all the links from the page
     const links = await page.$$eval("a", (tags) => {
       return tags.map((tag) => tag.href);
     });
-
+    // getting the site title
     const title = await page.$eval("title", (tag) => {
       return tag.innerText;
     });
 
+    // scrapps the description from the page
+    const description = await page.$eval("meta[name='description']", (meta) => {
+      return meta.name;
+    });
+
+    // gets all the keywords from the site
+    const keywords = await page.$eval("meta[name='keywords']", (meta) => {
+      return meta.content;
+    });
+
+    // gets the cover image
+    const coverImage = await page.$eval("meta[property='og:image']", (meta) => {
+      return meta.content;
+    });
+
+    // setting the scrapped data
+    scrappedData.title = title; // setting the title
+    scrappedData.links = links; // all the links found in the sites
+    scrappedData.link = currentLink; // sets the page's current link
+    scrappedData.coverImage = coverImage; // setting the site's image
+    //   closing the page
     page.close();
 
+    // closing the browser
     browser.close();
 
     return scrappedData;
