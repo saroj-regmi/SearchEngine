d2c0c6f8e1716eb909e66cab3799b12fb8ee52d9
heckerbl <sarojregmi.official@gmail.com>
2022-12-05 21:39:26 +0545

    added grammer

--- web-crawler/controller/site parser/generateKeywords.js
+++ web-crawler/controller/site parser/generateKeywords.js
@@ -1,12 +1,35 @@
 import extractFullText from "./extractFullText.js";
 
+// grammer stuff
+// import prepositions from "./grammer/preposition.json";
+// import pronouns from "./grammer/pronoun.json";
+import { readFile } from "fs/promises";
+
+// all grammer prepositions
+const preposition = JSON.parse(
+  await readFile(new URL("./grammer/preposition.json", import.meta.url))
+);
+const pronouns = JSON.parse(
+  await readFile(new URL("./grammer/pronoun.json", import.meta.url))
+);
+const commonWords = JSON.parse(
+  await readFile(new URL("./grammer/common.json"))
+)
+
 const extractKeyword = (array) => {
   const keywords = [];
 
   for (var i in array) {
     const currentItem = array[i].slice();
+
+    // removing all the pronouns and prepositions
+    if (
+      (preposition && preposition.includes(currentItem)) ||
+      (pronouns && pronouns.includes(currentItem))
+    )
+      continue;
+
     let repeat = 0;
-    console.log(currentItem);
     for (var j in array) {
       if (currentItem === array[j].slice()) repeat++;
       if (repeat > 3) {
@@ -15,6 +38,7 @@ const extractKeyword = (array) => {
       }
     }
   }
+
   return keywords;
 };
 
@@ -23,7 +47,9 @@ const generateKeywords = async (page) => {
 
   const textArray = fullText.split(" ");
   const keywords = extractKeyword(textArray);
+
   console.log(keywords);
+  return keywords;
 };
 
 export default generateKeywords;
