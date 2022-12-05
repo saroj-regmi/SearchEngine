1f8f8e84ac3f428ab247401d36d80cd1a3aedd98
heckerbl <sarojregmi.official@gmail.com>
2022-12-05 22:59:53 +0545

    UPDATE: added a rule for checking crawlable links

---
+++ web-crawler/controller/site parser/checkRobots.js
@@ -0,0 +1,18 @@
+import robotsParser from "robots-txt-parser";
+const robots = robotsParser();
+ 
+const checkRobots = async (url) => {
+  try {
+    await robots.useRobotsFor(url);
+    if (await robots.canCrawl(url)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export default checkRobots;
