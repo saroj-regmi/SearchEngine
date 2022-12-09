ece1c6387e5ac208f7707ccd514d51955e71c167
heckerbl <sarojregmi.official@gmail.com>
2022-12-09 20:29:53 +0545

    API: sites controller

---
+++ api/controllers/_site.js
@@ -0,0 +1,38 @@
+// fetches all the sites
+export const getAllSites = async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({
+      msg: "Error occured in the server while getting all the sites",
+    });
+  }
+};
+
+// updates one site
+export const updateSite = async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({
+      msg: "Error occured in the server while updating the site",
+    });
+  }
+};
+
+// deleting all the sites
+export const deleteAllSites = async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({
+      msg: "Error occured in the server while deleting all the sites",
+    });
+  }
+};
+
+export const searchSite = async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({
+      msg: "Error occured in the server while searching the site ",
+    });
+  }
+};
--- api/routes/siteRoutes.js
+++ api/routes/siteRoutes.js
@@ -2,17 +2,17 @@ import express from "express";
 
 // getting the controllers
 import {
-  getAllRoutes,
-  changeRouteStatus,
-  deleteAllRoutes,
+  getAllSites,
+  updateSite,
+  deleteAllSites,
 } from "../controllers/_site.js";
 
 // creating the router
 const siteRouter = express.Router();
 
 // routes
-siteRouter.route("/").get(getAllRoutes);
-siteRouter.route("/changeStatus").patch(changeRouteStatus);
-siteRouter.route("/delete").delete(deleteAllRoutes);
+siteRouter.route("/").get(getAllSites);
+siteRouter.route("/changeStatus").patch(updateSite);
+siteRouter.route("/delete").delete(deleteAllSites);
 
 export default siteRouter;
